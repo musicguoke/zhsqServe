87e4f9002636a272f8e77c9b25ad56c78bf66165
Justin5202 <386659643@qq.com>
2018-05-23 17:37:10 +0800

    new system interface

---
+++ src/api/system.js
@@ -0,0 +1,33 @@
+import axios from '@/util/http'
+import qs from 'qs'
+import { url } from './config.js'
+
+axios.defaults.withCredentials = true
+
+export function getAreaQx() {
+  const data = {
+    pageNo: 1,
+    pageSize: 50,
+    method: 'list'
+  }
+
+  return axios.post(`${url}/sys/areaQx/list.do`, qs.stringify(data)).then(res => {
+      return Promise.resolve(res.data)
+  })
+}
+
+export function getDateTree(id) {
+  const data = {
+    parentid: id || 'G00000000'
+  }
+
+  return axios.post(`${url}/sys/msTabDataController/getGroupDataTreeByParentid.do`, qs.stringify(data)).then(res => {
+    return Promise.resolve(res.data)
+  })
+}
+
+export function getMapConfig() {
+  return axios.post(`${url}/sys/msMapConfigController/list.do`).then(res => {
+    return Promise.resolve(res.data)
+  })
+}
