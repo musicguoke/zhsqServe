f3cbeaa71e5edfb545f906571f742e831fe8c541
Justin5202 <386659643@qq.com>
2018-06-15 17:17:16 +0800

    modify

--- src/page/resource/resource-manage/resource-item/mapConfigure.vue
+++ src/page/resource/resource-manage/resource-item/mapConfigure.vue
@@ -1,186 +1,187 @@
 <template>
-    <div>
+    <div class="map-config">
         <v-search :importShow="false" :searchShow="false" :disabled="selectedId.length <= 0" @on-delete="deleteMany" @on-build="openAddModal" />
-        <el-table :data="mapConfigureData" border style="width: 100%"  @selection-change="handleSelectionChange">
-            <el-table-column type="selection" width="55">
-            </el-table-column>
-            <el-table-column prop="id" label="ID" width="100" sortable>
-            </el-table-column>
-            <el-table-column prop="mName" label="名称">
-            </el-table-column>
-            <el-table-column prop="mUrl" label="地图地址" width="300">
-            </el-table-column>
-            <el-table-column prop="mImage" label="地图图例" width="300">
-            </el-table-column>
-            <el-table-column prop="mVersion" label="版本" sortable>
-            </el-table-column>
-            <el-table-column label="操作" width="120" align="center">
-                <template slot-scope="scope">
-                        <Button type="info"  size="small"  @click="openEditModal(scope)">编辑</Button>
+        <div class="tableSize">
+            <el-table :data="mapConfigureData" border style="width: 100%" @selection-change="handleSelectionChange">
+                <el-table-column type="selection" width="55">
+                </el-table-column>
+                <el-table-column prop="id" label="ID" width="60" sortable>
+                </el-table-column>
+                <el-table-column prop="mName" label="名称" width="100">
+                </el-table-column>
+                <el-table-column prop="mUrl" label="地图地址">
+                </el-table-column>
+                <el-table-column prop="mImage" label="地图图例">
+                </el-table-column>
+                <el-table-column prop="mVersion" label="版本" width="80" sortable>
+                </el-table-column>
+                <el-table-column label="操作" width="120" align="center">
+                    <template slot-scope="scope">
+                        <Button type="primary" size="small" @click="openEditModal(scope)">编辑</Button>
                         <Button type="error" size="small" @click="remove(scope)">删除</Button>
-                </template>
-            </el-table-column>
-        </el-table>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
         <div class="tablePage">
             <Page :total="pageLength" @on-change="pageChange" v-show="pageLength > 10" show-total show-elevator></Page>
         </div>
         <Modal v-model="mapConfigureModal" :title=modalTitle @on-ok="addOrUpdate">
             <Form :model="mapConfigureForm" :label-width="80">
                 <FormItem label="名称">
-                    <Input v-model="mapConfigureForm.mName" ></Input>
+                    <Input v-model="mapConfigureForm.mName"></Input>
                 </FormItem>
                 <FormItem label="地图地址">
-                    <Input v-model="mapConfigureForm.mUrl" ></Input>
+                    <Input v-model="mapConfigureForm.mUrl"></Input>
                 </FormItem>
                 <FormItem label="排序">
-                    <Input v-model="mapConfigureForm.mOrder" ></Input>
+                    <Input v-model="mapConfigureForm.mOrder"></Input>
                 </FormItem>
-                <FormItem label="地图图例"> 
+                <FormItem label="地图图例">
                     <Upload :action="`${uploadUrl}/sys/file/upload.do`" with-credentials :on-success="handleSuccessUpload" accept=".jpg,.png" :max-size="2048" ref="upload">
                         <Button type="ghost" icon="ios-cloud-upload-outline">请选择</Button>
-                         <div slot="tip" >只能上传jpg/png文件，且不超过2M</div>
+                        <div slot="tip">只能上传jpg/png文件，且不超过2M</div>
                     </Upload>
                 </FormItem>
                 <FormItem label="图例地址">
-                    <Input v-model="mapConfigureForm.mImage" ></Input>
+                    <Input v-model="mapConfigureForm.mImage"></Input>
                 </FormItem>
                 <FormItem label="版本">
-                    <Input v-model="mapConfigureForm.mVersion" ></Input>
+                    <Input v-model="mapConfigureForm.mVersion"></Input>
                 </FormItem>
             </Form>
         </Modal>
     </div>
 </template>
 <script>
-import {getMapConfigure,insertMapConfigure,updateMapConfigure,deleteMapConfigureById,deletesMapConfigureById} from '@/api/dataSource-service'
+import { getMapConfigure, insertMapConfigure, updateMapConfigure, deleteMapConfigureById, deletesMapConfigureById } from '@/api/dataSource-service'
 import vSearch from '@/components/search/index'
 import { url } from '@/api/config.js'
 export default {
     components: {
         vSearch
     },
-    data(){
-        return{
-            searchName: '',
+    data() {
+        return {
             searchType: 1,
-            pageLength:0,
-            isAdd:true,
-            modalTitle:'',
-            mapConfigureData:[],
-            mapConfigureModal:false,
-            uploadUrl:url,
-            mapConfigureForm:{
-                id:'',
-                mName:'',
-                mUrl:'',
-                mOrder:'',
-                mImage:'',
-                mVersion:''
+            pageLength: 0,
+            isAdd: true,
+            modalTitle: '',
+            mapConfigureData: [],
+            mapConfigureModal: false,
+            uploadUrl: url,
+            mapConfigureForm: {
+                id: '',
+                mName: '',
+                mUrl: '',
+                mOrder: '',
+                mImage: '',
+                mVersion: ''
             },
-            nowPage:1,
-            selectedId:[]
+            nowPage: 1,
+            selectedId: []
         }
     },
-    created(){
+    created() {
         this._getMapConfigure(1)
     },
-    methods:{
+    methods: {
         //地图配置
-        _getMapConfigure(page){
-            let data ={
-                pageNo:page,
-                pageSize:10
+        _getMapConfigure(page) {
+            let data = {
+                pageNo: page,
+                pageSize: 10
             }
-            getMapConfigure(data).then(res=>{
+            getMapConfigure(data).then(res => {
                 this.pageLength = res.data.total
                 this.mapConfigureData = res.data.list
             })
         },
         //分页点击
-        pageChange(Page){
+        pageChange(Page) {
             this.nowPage = Page
             this._getAreaText(Page)
         },
         //打开新增模态框
-        openAddModal(){
+        openAddModal() {
             this.isAdd = true
             this.mapConfigureModal = true
-            for(let i in this.mapConfigureForm){
+            for (let i in this.mapConfigureForm) {
                 this.mapConfigureForm[i] = ''
             }
-            if(this.$refs.upload._data.fileList){
+            if (this.$refs.upload._data.fileList) {
                 this.$refs.upload._data.fileList = []
             }
             this.modalTitle = '新增地图配置'
         },
         //打开编辑模态框
-        openEditModal(params){
+        openEditModal(params) {
             this.isAdd = false
             this.mapConfigureModal = true
-            for(let i in this.mapConfigureForm){
+            for (let i in this.mapConfigureForm) {
                 this.mapConfigureForm[i] = ''
-                if(params.row[i]){
+                if (params.row[i]) {
                     this.mapConfigureForm[i] = params.row[i]
                 }
-            } 
-            if(this.$refs.upload._data.fileList){
+            }
+            if (this.$refs.upload._data.fileList) {
                 this.$refs.upload._data.fileList = []
             }
-            this.modalTitle = '修改地图配置'  
+            this.modalTitle = '修改地图配置'
         },
         //点击确定
-        addOrUpdate(){
+        addOrUpdate() {
             let data = {}
-                data = {
-                    mName:this.mapConfigureForm.mName,
-                    mUrl:this.mapConfigureForm.mUrl,
-                    mOrder:this.mapConfigureForm.mOrder,
-                    mImage:this.mapConfigureForm.mImage,
-                    mVersion:this.mapConfigureForm.mVersion
-                }
-            if(this.isAdd){
-                insertMapConfigure(data).then(res=>{
-                    if(res.code == 20000){
+            data = {
+                mName: this.mapConfigureForm.mName,
+                mUrl: this.mapConfigureForm.mUrl,
+                mOrder: this.mapConfigureForm.mOrder,
+                mImage: this.mapConfigureForm.mImage,
+                mVersion: this.mapConfigureForm.mVersion
+            }
+            if (this.isAdd) {
+                insertMapConfigure(data).then(res => {
+                    if (res.code == 20000) {
                         this._getMapConfigure(this.nowPage)
                         this.$Message.success('添加成功');
-                    }else{
+                    } else {
                         this.$Message.error(res.message)
                     }
                 })
-            }else{
+            } else {
                 data.id = this.mapConfigureForm.id
                 updateMapConfigure(data).then(res => {
-                    if(res.code == 20000){
+                    if (res.code == 20000) {
                         this._getMapConfigure(this.nowPage)
                         this.$Message.success('修改成功');
-                    }else{
+                    } else {
                         this.$Message.error(res.message)
                     }
                 })
             }
         },
         //删除
-        remove (params) {
+        remove(params) {
             let data = {}
             this.$Modal.confirm({
                 content: '删除后数据无法恢复，是否继续？',
                 onOk: () => {
-                    data = {id:params.row.id}
-                    deleteMapConfigureById(data).then(res=>{
-                    if(res.code == 20000){
-                            this.mapConfigureData.splice(params.$index,1)
+                    data = { id: params.row.id }
+                    deleteMapConfigureById(data).then(res => {
+                        if (res.code == 20000) {
+                            this.mapConfigureData.splice(params.$index, 1)
                             this._getMapConfigure(this.nowPage)
                             this.$Message.success('删除成功');
                         }
                     })
                 },
-                onCancel: () => {            
+                onCancel: () => {
                 }
-            });   
+            });
         },
         _deletesMapConfigureById(id) {
             let data = {
-                ids:id
+                ids: id
             }
             deletesMapConfigureById(data).then(res => {
                 if (res.code === 20000) {
@@ -206,11 +207,11 @@ export default {
                 onCancel: () => { }
             })
         },
-        handleSuccessUpload(data){
-            if(data.code == 20000){
+        handleSuccessUpload(data) {
+            if (data.code == 20000) {
                 this.mapConfigureForm.mImage = data.data
                 this.$Message.success(data.message)
-            }else{
+            } else {
                 this.$Message.error(data.message)
             }
         }
@@ -218,6 +219,5 @@ export default {
 }
 </script>
 <style>
-
 </style>
 
